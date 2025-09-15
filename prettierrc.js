f10faa3c24052d4224beda1810d23a0ccef1609a
LarissaSarmento <larissa.altoe@atualsistemas.local>
2025-09-15 14:06:00 -0300

    Vite-Prettier-Eslint

---
+++ prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    semi: true, // Adiciona o ; no final
+    trailingComma: 'all', 
+    singleQuote: true, //Colocar sempre aspas simples 
+    printWhidth: 100, // Tamanho do código até querbrar de linha
+    tabWidth: 2, // Tamanho da TAB
+}
