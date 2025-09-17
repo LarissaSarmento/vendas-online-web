db5f672fd61a3654ee367c95fd210418b63f98fb
LarissaSarmento <larissalicurgoaltoe@outlook.com>
2025-09-16 23:09:52 -0300

    UsoSyleComponent

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,10 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 import styled from 'styled-components'
+import { Button } from 'antd'
 
 
-function App() {
+/*function App() {
   const [count, setCount] = useState(0)
 
   return (
@@ -20,10 +21,10 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <StyledLink className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+      <StyledLink isBlue={count > 4} className="card">
+        <Button type="primary"onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
+        </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
@@ -35,9 +36,46 @@ function App() {
   )
 }
 
-const StyledLink = styled.div`
-  color: pink;
+interface  PStyledLink{
+  isBlue?: boolean
+}
+
+const StyledLink = styled.div<PStyledLink>`
+  color: ${(props)=> (props.isBLue ? 'blue' : 'pink')};
   font-weight:bold;
 `;
+
+*/
+
+// const MyDiv = (props) => { Possofazer assim também como abaixo
+const myDiv = ({ titulo }) => {
+
+  // console.log('props', props)
+    return (
+         <div>
+           <h1>{titulo}</h1> 
+            {/* <h1>{props.titulo}</h1> Posso fazer assim também */}
+           <h2>Subtitulo</h2>
+         </div>
+    )
+}
+
+function App(){
+    return (
+        <div>
+            <div>
+                <myDiv titulo="Larissa"/>
+            </div>
+            <header>
+                <h1>Titulo</h1>
+                <h2>Subtitulo</h2>
+            </header>
+            <div>
+                <p>Lorem, ipsum dolor sit amet consectetur
+                     adipisicing elit. Dolor ratione expedita consequatur deserunt ipsa minima impedit nihil fugit sunt officiis esse neque, ab beatae corporis quidem aspernatur pariatur odit ipsum?</p>
+            </div>
+        </div>
+    )
+}
 
 export default App
---
+++ src/App2.tsx
@@ -0,0 +1,32 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+// // import LoginScreen from './modules/login'
+
+// const MyDiv = () => {
+//     return (
+//          <h1>Titulo</h1>
+//          <h2>Subtitulo</h2>
+//     )
+// }
+
+// function App(){
+//     return (
+//         <div>
+//             <div>
+//                 <MyDiv />
+//             </div>
+//             <header>
+//                 <h1>Titulo</h1>
+//                 <h2>Subtitulo</h2>
+//             </header>
+//             <div>
+//                 <p>Lorem, ipsum dolor sit amet consectetur
+//                      adipisicing elit. Dolor ratione expedita consequatur deserunt ipsa minima impedit nihil fugit sunt officiis esse neque, ab beatae corporis quidem aspernatur pariatur odit ipsum?</p>
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default App
