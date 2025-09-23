a4c8c05e3952f6f6d676f2b3d3c3964791244417
LarissaSarmento <larissa.altoe@atualsistemas.local>
2025-09-23 16:56:20 -0300

    login

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-import styled from 'styled-components'
-import { Button } from 'antd'
-
+import LoginScreen from "./modules"
 
   function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <StyledLink isBlue={count > 4} className="card">
-        <Button type="primary"onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </Button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </StyledLink>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-interface  PStyledLink{
-  isBlue?: boolean
-}
-
-const StyledLink = styled.div<PStyledLink>`
-  color: ${(props)=> (props.isBLue ? 'blue' : 'pink')};
-  font-weight:bold;
-`;
+  return <LoginScreen />
+  }
 
 
 export default App
---
+++ src/modules/screens/loginScreen.tsx
@@ -0,0 +1,9 @@
+import { BackgroundImage } from "../styles/LoginScreen.styles"
+
+const LoginScreen = ()=>{
+    return (<div>
+        <BackgroundImage src="./background.png" />
+    </div>)
+}
+
+export default LoginScreen
