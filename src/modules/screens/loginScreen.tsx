81546550e2981ed8b9b13cf50bb6503dbaf005a5
LarissaSarmento <larissalicurgoaltoe@outlook.com>
2025-09-24 23:21:18 -0300

    pratica240925

--- src/modules/screens/loginScreen.tsx
+++ src/modules/screens/loginScreen.tsx
@@ -1,8 +1,11 @@
-import { BackgroundImage } from "../styles/LoginScreen.styles"
+import { BackgroundImage, ContainerLogin, LogoImage } from "../styles/LoginScreen.styles"
 
 const LoginScreen = ()=>{
     return (<div>
         <BackgroundImage src="./background.png" />
+        <ContainerLogin>
+            <LogoImage src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fbr.freepik.com%2Ffotos-vetores-gratis%2Flogo-png&psig=AOvVaw1SGsbzOW3bPQQbyxh-rZod&ust=1758851068584000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCPC30fvk8o8DFQAAAAAdAAAAABAE"/>
+        </ContainerLogin>
     </div>)
 }
 
--- src/modules/styles/LoginScreen.styles.ts
+++ src/modules/styles/LoginScreen.styles.ts
@@ -7,4 +7,20 @@ top: 0;
 width: 100%;
 height: 100vh;
 object-fit: cover;
+z-index: 1;
+`
+
+export const LogoImage = styled.img`
+width:20%
+`
+
+export const ContainerLogin = styled.div`
+background-color: #ffffffff;
+width: 100%;
+height: 100vh;
+max-width: 646px;
+position: absolute;
+right:0;
+top:0;  
+z-index: 2;
 `
