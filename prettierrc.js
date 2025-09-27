8e000ef6ab69923e57f9f111e49d7b30dd5dd2c3
LarissaSarmento <larissalicurgoaltoe@outlook.com>
2025-09-27 19:44:19 -0300

    creat Login Screen

--- prettierrc.js
+++ prettierrc.js
@@ -2,6 +2,6 @@ module.exports = {
     semi: true, // Adiciona o ; no final
     trailingComma: 'all', 
     singleQuote: true, //Colocar sempre aspas simples 
-    printWhidth: 100, // Tamanho do código até querbrar de linha
+    printWidth: 100, // Tamanho do código até querbrar de linha
     tabWidth: 2, // Tamanho da TAB
 }
--- src/modules/screens/loginScreen.tsx
+++ src/modules/screens/loginScreen.tsx
@@ -1,12 +1,16 @@
-import { BackgroundImage, ContainerLogin, LogoImage } from "../styles/LoginScreen.styles"
+import { BackgroundImage, ContainerLogin, ContainerLoginScreen, LimitedContainer, LogoImage } from "../styles/LoginScreen.styles"
 
 const LoginScreen = ()=>{
-    return (<div>
-        <BackgroundImage src="./background.png" />
+    return (
+    <ContainerLoginScreen>
         <ContainerLogin>
-            <LogoImage src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fbr.freepik.com%2Ffotos-vetores-gratis%2Flogo-png&psig=AOvVaw1SGsbzOW3bPQQbyxh-rZod&ust=1758851068584000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCPC30fvk8o8DFQAAAAAdAAAAABAE"/>
+            <LimitedContainer>
+            <LogoImage src="./logo.png"/>
+            </LimitedContainer>
         </ContainerLogin>
-    </div>)
+        <BackgroundImage src="./background.jpg" />
+    </ContainerLoginScreen>
+    )
 }
 
 export default LoginScreen
