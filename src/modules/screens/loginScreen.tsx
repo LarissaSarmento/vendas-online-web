ee66c203e35fdba0e595769adc38ffaf45090144
LarissaSarmento <larissalicurgoaltoe@outlook.com>
2025-10-08 22:17:40 -0300

    pratica08-10-2025

--- src/modules/screens/loginScreen.tsx
+++ src/modules/screens/loginScreen.tsx
@@ -3,15 +3,35 @@ import Input from "../../shared/inputs/input/input"
 import Button from "../../shared/buttons/button/Button"
 
 const LoginScreen = ()=>{
+    const [username, setUsername] = useState(''); // O useState é um array que na primeira posição no caso o username é o valor dele e na segunda é modificar o valor dele
+     const [password, setPassword] = useState('');
+
+    const handleUsername = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setUsername(event.target.value )
+    } 
+
+    const handlePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(event.target.value )
+    } 
+
+    const handleLogin = () => {
+        alert(
+        `username: ${username},
+        password: ${password},
+        `);
+    };
+
     return (
     <ContainerLoginScreen>
         <ContainerLogin>
-            <LimitedContainer>
+            <LimitedContainer> 
             <LogoImage src="./logo.png"/>
             <TitleLogin level={2} type="secondary">LOGIN</TitleLogin>
-            <Input title="Usuário" placeholder="Digite o usuário aqui" margin='32px 0px 0px'/>
-            <Input title="Senha" placeholder="Digite a senha aqui"  margin='32px 0px 0px'/>
-           <Button type="primary" margin='64px 0px 16px 0px'>Entrar</Button>
+            <Input title="Usuário" placeholder="Digite o usuário aqui" margin='32px 0px 0px' onChange={handleUsername} value={username}/>
+            <Input type="password" title="Senha" placeholder="Digite a senha aqui"  margin='32px 0px 0px'
+             onChange={handlePassword} value={password}/>
+           <Button type="primary" margin='64px 0px 16px 0px' onclick={handleLogin}>
+            Entrar</Button>
             </LimitedContainer>
         </ContainerLogin>
         <BackgroundImage src="./background.jpg" />
